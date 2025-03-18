77e75152bb29bd906bd2c0d9f804b4f5bf5d7809
Corinna Baksik <cbaksik@gmail.com>
2025-03-18 15:58:35 -0400

    add 880s

--- discovery-ve/lds31-place.vb
+++ discovery-ve/lds31-place.vb
@@ -50,3 +50,61 @@ rule "Primo VE - Lds31 - 370"
 		concatenate with delimiter (TEMP"1",TEMP"2","") 		
         create pnx."display"."lds31" with TEMP"1"
 end
+
+// begin 880
+
+rule "Primo VE - lds38 880-752"
+	when
+        MARC is "880" AND
+        MARC."880"."6" match "752-.*"
+	then
+		set TEMP"1" to MARC."880" sub without sorting "a,b,c,d" delimited by " -- "
+		remove substring using regex (TEMP"1","(;|,|\\.)+$")
+        add prefix (TEMP"1","Hierarchical: ")
+		set TEMP"2" to MARC."880" sub without sort "e"
+		add prefix (TEMP"2","[")
+		add suffix (TEMP"2","]")
+		concatenate with delimiter (TEMP"1",TEMP"2","")
+		add suffix (TEMP"1","$$Q")
+		set TEMP"3" to MARC."880" sub without sort "a-d,f-h"
+		concatenate with delimiter (TEMP"1",TEMP"3","")
+ 		create pnx."display"."lds31" with TEMP"1"
+end
+
+rule "Primo VE - lds38 880-751"
+	when
+        MARC is "880" AND
+        MARC."880"."6" match "751-.*"
+	then
+		set TEMP"1" to MARC."880" sub without sort "3,a,g,e" wrap subfields
+		replace wrapping delimiters (TEMP"1","3","",": ")
+		replace wrapping delimiters (TEMP"1","a","","")		
+		replace wrapping delimiters (TEMP"1","g"," -- ","")		
+		replace wrapping delimiters (TEMP"1","e"," [","]")
+		add prefix (TEMP"1","Geographic name: ")
+		add suffix (TEMP"1","$$Q")
+		set TEMP"2" to MARC."880" sub without sort "a,g"
+		concatenate with delimiter (TEMP"1",TEMP"2","")
+ 		create pnx."display"."lds31" with TEMP"1"
+end
+
+rule "Primo VE - lds38 880-370"
+	when
+        MARC is "880" AND
+        MARC."880"."6" match "370-.*"
+	then
+		set TEMP"1" to MARC."880" sub without sort "3,c-t" wrap subfields
+		replace wrapping delimiters (TEMP"1","3","",": ")
+		replace wrapping delimiters (TEMP"1","i","",": ")		
+		replace wrapping delimiters (TEMP"1","c","",". ")		
+		replace wrapping delimiters (TEMP"1","f","",". ")		
+		replace wrapping delimiters (TEMP"1","g","Place of origin of work or expression: ",". ")		
+		replace wrapping delimiters (TEMP"1","s","Start period: ",". ")		
+		replace wrapping delimiters (TEMP"1","t","End period: ",". ")	
+		replace string by string (TEMP"1","::",":")
+		replace string by string (TEMP"1","..",".")
+		add suffix (TEMP"1","$$Q")
+		set TEMP"2" to MARC."880" sub without sort "c,f,g"
+		concatenate with delimiter (TEMP"1",TEMP"2","") 		
+        create pnx."display"."lds31" with TEMP"1"
+end
