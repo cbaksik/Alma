4f1b083e2fc5555e212697a5ab788efb848a7b07
Corinna Baksik <cbaksik@gmail.com>
2025-03-24 16:37:44 -0400

    using addtitle instead of lds04 for 246

--- discovery-ve/addTitle-246.vb
+++ discovery-ve/addTitle-246.vb
@@ -1,47 +1,47 @@
-rule "Primo VE - Lds04"
+rule "Primo VE - addtitle 246"
 	when
 		MARC."246" has any "a-p" AND 
 		MARC."246".ind"1" equals " "
 	then
-		create pnx."display"."lds04" with MARC."246" sub without sort "a-p"
+		create pnx."display"."addtitle" with MARC."246" sub without sort "a-p"
 end
 
-rule "Primo VE - Lds04 - 1st ind 0"
+rule "Primo VE - addtitle - 1st ind 0"
 	when
 		MARC."246" has any "a-p" AND 
 		MARC."246".ind"1" equals "0"
 	then
-		create pnx."display"."lds04" with MARC."246" sub without sort "a-p"
+		create pnx."display"."addtitle" with MARC."246" sub without sort "a-p"
 end
 
-rule "Primo VE - Lds04 - 1st ind 3"
+rule "Primo VE - addtitle - 1st ind 3"
 	when
 		MARC."246" has any "a-p" AND 
 		MARC."246".ind"1" equals "3"
 	then
-		create pnx."display"."lds04" with MARC."246" sub without sort "a-p"
+		create pnx."display"."addtitle" with MARC."246" sub without sort "a-p"
 end
 
-rule "Primo VE - Lds04 - 1st 1, 2nd 3"
+rule "Primo VE - addtitle - 1st 1, 2nd 3"
 	when
 		MARC."246" has any "a-p" AND 
 		MARC."246".ind"1" equals "1" AND 
 		MARC."246".ind"2" equals "3"
 	then
-		create pnx."display"."lds04" with MARC."246" sub without sort "a-p"
+		create pnx."display"."addtitle" with MARC."246" sub without sort "a-p"
 end
 
-rule "Primo VE - Lds04 - 1st 1, 2nd null"
+rule "Primo VE - addtitle - 1st 1, 2nd null"
 	when
 		MARC."246" has any "a-p" AND 
 		MARC."246".ind"1" equals "1" AND 
 		MARC."246".ind"2" equals " "
 	then
-		create pnx."display"."lds04" with MARC."246" sub without sort "a-p"
+		create pnx."display"."addtitle" with MARC."246" sub without sort "a-p"
 end
 // begin section with prefixes
 
-rule "Primo VE - Lds04 - 1st 1, 2nd 0"
+rule "Primo VE - addtitle - 1st 1, 2nd 0"
 	when
 		MARC."246" has any "a-p" AND 
 		MARC."246".ind"1" equals "1" AND 
@@ -49,10 +49,10 @@ rule "Primo VE - Lds04 - 1st 1, 2nd 0"
 	then
 		set TEMP"1" to MARC."246" sub without sort "a-p"
 		add prefix (TEMP"1","Portion of title: ")
-		create pnx."display"."lds04" with TEMP"1"
+		create pnx."display"."addtitle" with TEMP"1"
 end
 
-rule "Primo VE - Lds04 - 1st 1, 2nd 1"
+rule "Primo VE - addtitle - 1st 1, 2nd 1"
 	when
 		MARC."246" has any "a-p" AND 
 		MARC."246".ind"1" equals "1" AND 
@@ -60,10 +60,10 @@ rule "Primo VE - Lds04 - 1st 1, 2nd 1"
 	then
 		set TEMP"1" to MARC."246" sub without sort "a-p"
 		add prefix (TEMP"1","Parallel title: ")
-		create pnx."display"."lds04" with TEMP"1"
+		create pnx."display"."addtitle" with TEMP"1"
 end
 
-rule "Primo VE - Lds04 - 1st 1, 2nd 2"
+rule "Primo VE - addtitle - 1st 1, 2nd 2"
 	when
 		MARC."246" has any "a-p" AND 
 		MARC."246".ind"1" equals "1" AND 
@@ -71,10 +71,10 @@ rule "Primo VE - Lds04 - 1st 1, 2nd 2"
 	then
 		set TEMP"1" to MARC."246" sub without sort "a-p"
 		add prefix (TEMP"1","Distinctive title: ")
-		create pnx."display"."lds04" with TEMP"1"
+		create pnx."display"."addtitle" with TEMP"1"
 end
 
-rule "Primo VE - Lds04 - 1st 1, 2nd 4"
+rule "Primo VE - addtitle - 1st 1, 2nd 4"
 	when
 		MARC."246" has any "a-p" AND 
 		MARC."246".ind"1" equals "1" AND 
@@ -82,10 +82,10 @@ rule "Primo VE - Lds04 - 1st 1, 2nd 4"
 	then
 		set TEMP"1" to MARC."246" sub without sort "a-p"
 		add prefix (TEMP"1","Cover title: ")
-		create pnx."display"."lds04" with TEMP"1"
+		create pnx."display"."addtitle" with TEMP"1"
 end
 
-rule "Primo VE - Lds04 - 1st 1, 2nd 5"
+rule "Primo VE - addtitle - 1st 1, 2nd 5"
 	when
 		MARC."246" has any "a-p" AND 
 		MARC."246".ind"1" equals "1" AND 
@@ -93,10 +93,10 @@ rule "Primo VE - Lds04 - 1st 1, 2nd 5"
 	then
 		set TEMP"1" to MARC."246" sub without sort "a-p"
 		add prefix (TEMP"1","Added title page title: ")
-		create pnx."display"."lds04" with TEMP"1"
+		create pnx."display"."addtitle" with TEMP"1"
 end
 
-rule "Primo VE - Lds04 - 1st 1, 2nd 6"
+rule "Primo VE - addtitle - 1st 1, 2nd 6"
 	when
 		MARC."246" has any "a-p" AND 
 		MARC."246".ind"1" equals "1" AND 
@@ -104,10 +104,10 @@ rule "Primo VE - Lds04 - 1st 1, 2nd 6"
 	then
 		set TEMP"1" to MARC."246" sub without sort "a-p"
 		add prefix (TEMP"1","Caption title: ")
-		create pnx."display"."lds04" with TEMP"1"
+		create pnx."display"."addtitle" with TEMP"1"
 end
 
-rule "Primo VE - Lds04 - 1st 1, 2nd 7"
+rule "Primo VE - addtitle - 1st 1, 2nd 7"
 	when
 		MARC."246" has any "a-p" AND 
 		MARC."246".ind"1" equals "1" AND 
@@ -115,10 +115,10 @@ rule "Primo VE - Lds04 - 1st 1, 2nd 7"
 	then
 		set TEMP"1" to MARC."246" sub without sort "a-p"
 		add prefix (TEMP"1","Running title: ")
-		create pnx."display"."lds04" with TEMP"1"
+		create pnx."display"."addtitle" with TEMP"1"
 end
 
-rule "Primo VE - Lds04 - 1st 1, 2nd 8"
+rule "Primo VE - addtitle - 1st 1, 2nd 8"
 	when
 		MARC."246" has any "a-p" AND 
 		MARC."246".ind"1" equals "1" AND 
@@ -126,30 +126,30 @@ rule "Primo VE - Lds04 - 1st 1, 2nd 8"
 	then
 		set TEMP"1" to MARC."246" sub without sort "a-p"
 		add prefix (TEMP"1","Spine title: ")
-		create pnx."display"."lds04" with TEMP"1"
+		create pnx."display"."addtitle" with TEMP"1"
 end
 
-rule "Primo VE - Lds04 - 247"
+rule "Primo VE - addtitle - 247"
 	when
 		MARC."247" has any "a-p"
 	then
-		create pnx."display"."lds04" with MARC."247" sub without sort "a-p"
+		create pnx."display"."addtitle" with MARC."247" sub without sort "a-p"
 end
 
 // begin 880
 
-rule "Primo VE - Lds04 - 246-880"
+rule "Primo VE - addtitle - 246-880"
 	when
 		MARC."880" has any "a-p" AND
 		MARC."880"."6" match "246-.*"  
 	then
-		create pnx."display"."lds04" with MARC "880" sub without sort "a-p"
+		create pnx."display"."addtitle" with MARC "880" sub without sort "a-p"
 end
 
-rule "Primo VE - Lds04 - 247-880"
+rule "Primo VE - addtitle - 247-880"
 	when
 		MARC."880" has any "a-p" AND
 		MARC."880"."6" match "247-.*"  
 	then
-		create pnx."display"."lds04" with MARC "880" sub without sort "a-p"
+		create pnx."display"."addtitle" with MARC "880" sub without sort "a-p"
 end
