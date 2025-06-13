9cedf9e3260a9559e85e3c67ea24dcf8de3baeef
Corinna Baksik <cbaksik@gmail.com>
2025-06-13 15:36:18 -0400

    corrections, making lowercase to remove duplicate headings

--- discovery-ve/genre.vb
+++ discovery-ve/genre.vb
@@ -3,14 +3,17 @@ rule "Primo VE - Genre 600"
 		MARC."655" has any "a-z" AND
 		MARC."655".ind"2" equals "0"
 	then
-		set TEMP"1" to MARC."655" subfields "a-u,w" delimited by " " remove substring using regex "\\.+$"
-		set TEMP"2" to MARC."655" sub without sorting "v,x-z" delimited by " -- "
+		set TEMP"1" to MARC."655" subfields "3,a-z" delimited by " -- " remove substring using regex "\\.+$"
+		lower case (TEMP"1")
+		add suffix (TEMP"1","$$Q")
+		set TEMP"2" to MARC."655" sub without sort "a"
+		lower case (TEMP"2")
 		remove substring using regex (TEMP"2","\\.+$")
-		concatenate with delimiter (TEMP"1",TEMP"2"," -- ")
+		concatenate with delimiter (TEMP"1",TEMP"2","")
 		create pnx."display"."genre" with TEMP"1"
 end
 
-rule "Primo VE - Genre 655 sf u"
+rule "Primo VE - Genre 655 sf 7"
 	when
 		MARC."655" has any "a-z" AND
 		MARC."655".ind"2" equals "7" AND NOT
@@ -35,10 +38,13 @@ rule "Primo VE - Genre 655 sf u"
 		MARC."655"."2" match "tgfc" OR
 		MARC."655"."2" match "tpro")
 	then
-		set TEMP"1" to MARC."655" subfields "a-u,w" delimited by " " remove substring using regex "\\.+$"
-		set TEMP"2" to MARC."655" sub without sorting "v,x-z" delimited by " -- "
+		set TEMP"1" to MARC."655" subfields "3,a-z" delimited by " -- " remove substring using regex "\\.+$"
+		lower case (TEMP"1")
+		add suffix (TEMP"1","$$Q")
+		set TEMP"2" to MARC."655" sub without sort "a"
+		lower case (TEMP"2")
 		remove substring using regex (TEMP"2","\\.+$")
-		concatenate with delimiter (TEMP"1",TEMP"2"," -- ")
+		concatenate with delimiter (TEMP"1",TEMP"2","")
 		create pnx."display"."genre" with TEMP"1"
 end
 
@@ -47,9 +53,10 @@ rule "Primo VE - Genre 880-655"
 		MARC is "880"."v" AND
 		MARC."880"."6" match "655-.*"
 	then
-		set TEMP"1" to MARC."880" subfields "a-u,w" delimited by " " remove substring using regex "\\.+$"
-		set TEMP"2" to MARC."880" sub without sorting "v,x-z" delimited by " -- "
+		set TEMP"1" to MARC."880" subfields "3,a-z" delimited by " -- " remove substring using regex "\\.+$"
+		add suffix (TEMP"1","$$Q")
+		set TEMP"2" to MARC."880" sub without sort "a-z"
 		remove substring using regex (TEMP"2","\\.+$")
-		concatenate with delimiter (TEMP"1",TEMP"2"," -- ")
+		concatenate with delimiter (TEMP"1",TEMP"2","")
 		create pnx."display"."genre" with TEMP"1"
 end
