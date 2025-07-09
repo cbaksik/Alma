08f9eda17428b8bd0cac513f78b3ad80fbab5455
Corinna Baksik <cbaksik@gmail.com>
2025-07-09 10:37:00 -0400

    adding 130a to index

--- discovery-ve/lds61-lsr61-245k-222.vb
+++ discovery-ve/lds61-lsr61-245k-222.vb
@@ -12,4 +12,11 @@ rule "Primo VE Marc - Lsr61 222"
 		create pnx."search"."lsr61" with MARC "222"."a"
 end
 
+rule "Primo VE Marc - Lsr61 130 serials"
+	when
+		MARC.control."LDR"(6-8) matches "ai|as" AND
+		MARC is "130"."a"
+	then
+		create pnx."search"."lsr61" with MARC "130"."a"
+end
 
