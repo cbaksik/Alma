56fa9bcf3b31618c11214a66d4bb2294ddd08d15
Corinna Baksik <cbaksik@gmail.com>
2025-07-09 10:36:39 -0400

    DB category and best bets work

--- discovery-ve/lds39-db-category.vb
+++ discovery-ve/lds39-db-category.vb
@@ -1,7 +1,11 @@
 rule "Primo VE - Lds39"
       when
-            MARC."916" has any "a"
+		MARC."916" has any "a"
       then
-            create pnx."display"."lds39" with MARC "916" subfields "a" append translation "a"
+		set TEMP"1" to MARC."916" sub without sort "a"
+		add prefix (TEMP"1","$$T")
+		add suffix (TEMP"1","$$")
+		concatenate with delimiter (TEMP"1",TEMP"1","$$Q")
+		create pnx."display"."lds39" with TEMP"1"
 end
 
