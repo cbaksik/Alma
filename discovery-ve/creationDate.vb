c23e73dc1b9cca2b31e9efbff8d2c635829f8f18
Corinna Baksik <cbaksik@gmail.com>
2025-07-17 15:35:29 -0400

    adding priorities

--- discovery-ve/creationDate.vb
+++ discovery-ve/creationDate.vb
@@ -1,4 +1,5 @@
 rule "Primo VE - Date Of Publication 008"
+	priority 30
 	when
 		MARC is "260"."c" AND
 		MARC."260".ind"1"  equals "3"
@@ -9,6 +10,7 @@ rule "Primo VE - Date Of Publication 008"
 end
 
 rule "Primo VE - Date Of Publication 264/3/1"
+	priority 40
 	when
 		MARC is "264"."c" AND
 		MARC."264".ind"1"  equals "3" AND
@@ -20,6 +22,7 @@ rule "Primo VE - Date Of Publication 264/3/1"
 end
 
 rule "Primo VE - Date Of Publication 260"
+	priority 50
 	when
 		MARC is "260"."c"
 	then
@@ -29,6 +32,7 @@ rule "Primo VE - Date Of Publication 260"
 end
 
 rule "Primo VE - Date Of Publication 264"
+	priority 60
 	when
 		MARC is "264"."c"
 	then
@@ -38,8 +42,9 @@ rule "Primo VE - Date Of Publication 264"
 end
 
 rule "Primo VE - Date of Publication 008 real one"
+	priority 70
 	when
-		(MARC.control."008"(06-07) equals "b" OR
+		MARC.control."008"(06-07) equals "b" OR
 		MARC.control."008"(06-07) equals "e" OR
 		MARC.control."008"(06-07) equals "n" OR
 		MARC.control."008"(06-07) equals "p" OR
@@ -49,8 +54,7 @@ rule "Primo VE - Date of Publication 008 real one"
 		MARC.control."008"(06-07) equals "t" OR
 		MARC.control."008"(06-07) equals "|" OR
 		MARC.control."008"(06-07) equals " " OR
-		MARC.control."008"(06-07) equals "^") AND NOT
-		(MARC is "260"."c" OR MARC is "264"."c")
+		MARC.control."008"(06-07) equals "^"
 	then
 		set TEMP"1" to MARC.control."008".Date1
 		replace string by string (TEMP"1","u{4}","?")
@@ -60,13 +64,13 @@ rule "Primo VE - Date of Publication 008 real one"
 end
 
 rule "Primo VE - Date of Publication 008 contins"
+	priority 80
 	when
-		(MARC.control."008"(06-07) equals "c" OR
+		MARC.control."008"(06-07) equals "c" OR
 		MARC.control."008"(06-07) equals "i" OR
 		MARC.control."008"(06-07) equals "k" OR
 		MARC.control."008"(06-07) equals "m" OR
-		MARC.control."008"(06-07) equals "u")  AND NOT
-		(MARC is "260"."c" OR MARC is "264"."c")
+		MARC.control."008"(06-07) equals "u"
 	then
 		set TEMP"1" to MARC.control."008".Date1
 		set TEMP"2" to MARC.control."008".Date2		
@@ -81,9 +85,9 @@ rule "Primo VE - Date of Publication 008 contins"
 end
 
 rule "Primo VE - Date of Publication 008 ceased"
+	priority 90
 	when
-		MARC.control."008"(06-07) equals "d" AND NOT
-		(MARC is "260"."c" OR MARC is "264"."c")
+		MARC.control."008"(06-07) equals "d"
 	then
 		set TEMP"1" to MARC.control."008".Date1
 		set TEMP"2" to MARC.control."008".Date2		
