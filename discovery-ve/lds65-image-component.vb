b10b9675aee6bd4d4f4151f2dcf349d810b1c469
Corinna Baksik <cbaksik@gmail.com>
2025-08-29 16:16:19 -0400

    Update lds65-image-component.vb

--- discovery-ve/lds65-image-component.vb
+++ discovery-ve/lds65-image-component.vb
@@ -1,6 +1,7 @@
 rule "Primo VE - Lds65"
 	when
-		MARC."599" has any "a-z"
+		MARC."599" has any "a-z"  AND
+		MARC."599".ind"2"  equals "9"
 	then
 		set TEMP"1" to MARC."599" sub without sort "0-9,a-z" wrap subfields
 		replace wrapping delimiters (TEMP"1","0","==0","")
@@ -17,18 +18,24 @@ rule "Primo VE - Lds65"
 		replace wrapping delimiters (TEMP"1","b","==B","")
 		replace wrapping delimiters (TEMP"1","c","==C","")
 		replace wrapping delimiters (TEMP"1","d","==D","")
+		replace wrapping delimiters (TEMP"1","e","==E","")
 		replace wrapping delimiters (TEMP"1","f","==F","")
 		replace wrapping delimiters (TEMP"1","g","==G","")
 		replace wrapping delimiters (TEMP"1","h","==H","")
 		replace wrapping delimiters (TEMP"1","i","==I","")
 		replace wrapping delimiters (TEMP"1","j","==J","")
 		replace wrapping delimiters (TEMP"1","k","==K","")
+		replace wrapping delimiters (TEMP"1","l","==L","")
 		replace wrapping delimiters (TEMP"1","m","==M","")
 		replace wrapping delimiters (TEMP"1","n","==N","")
+		replace wrapping delimiters (TEMP"1","o","==O","")
+		replace wrapping delimiters (TEMP"1","p","==P","")
+		replace wrapping delimiters (TEMP"1","q","==Q","")
 		replace wrapping delimiters (TEMP"1","r","==R","")
 		replace wrapping delimiters (TEMP"1","s","==S","")
 		replace wrapping delimiters (TEMP"1","t","==T","")
 		replace wrapping delimiters (TEMP"1","u","==U","")
+		replace wrapping delimiters (TEMP"1","v","==V","")
 		replace wrapping delimiters (TEMP"1","w","==W","")
 		replace wrapping delimiters (TEMP"1","x","==X","")
 		replace wrapping delimiters (TEMP"1","y","==Y","")
