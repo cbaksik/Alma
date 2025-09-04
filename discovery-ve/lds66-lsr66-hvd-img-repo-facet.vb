a020363a4b49527d232bbcb7f3808cec414ed9a3
Corinna Baksik <cbaksik@gmail.com>
2025-09-04 18:49:23 -0400

    add translate delimiters

--- discovery-ve/lds66-lsr66-hvd-img-repo-facet.vb
+++ discovery-ve/lds66-lsr66-hvd-img-repo-facet.vb
@@ -4,6 +4,8 @@ rule "Primo VE Marc - Lsr66"
 		MARC."590".ind"2"  equals "9"
 	then
 		set TEMP"1" to MARC "590" sub without sort "f"
+		add prefix (TEMP"1","$$T")
+		add suffix (TEMP"1","$$")
 		create pnx."search"."lsr66" with  TEMP"1"
 end
 
