a9e4f89f098117f8838f8fe47e8c55af3cafc5b5
Corinna Baksik <cbaksik@gmail.com>
2025-09-19 16:39:54 -0400

    use code so we can change label in UI

--- discovery-ve/lds68-lsr68-original-image-facet.vb
+++ discovery-ve/lds68-lsr68-original-image-facet.vb
@@ -4,10 +4,10 @@ rule "Primo VE Marc - Lsr68"
 		MARC."590".ind"2"  equals "9" AND
 		MARC."590"."c" match "true"
 	then
-		set TEMP"1" to MARC."590" sub without sort "b"
-		add prefix (TEMP"1","[")
-		add suffix (TEMP"1"," images]")
-		create pnx."search"."lsr68" with "Yes"
+		set TEMP"1" to MARC."590" sub without sort "c"
+		add prefix (TEMP"1","$$T")
+		add suffix (TEMP"1","$$")
+		create pnx."search"."lsr68" with "hvd-img-orig-true"
 end
 
 
