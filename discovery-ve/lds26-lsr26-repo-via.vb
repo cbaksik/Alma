ce4a2100fc364931266f5ffdb178ec946c488efe
Corinna Baksik <cbaksik@gmail.com>
2025-09-19 16:40:51 -0400

    include component repos in index

--- discovery-ve/lds26-lsr26-repo-via.vb
+++ discovery-ve/lds26-lsr26-repo-via.vb
@@ -7,5 +7,22 @@ rule "Primo VE Marc - Lsr26"
 		create pnx."search"."lsr26" with TEMP"1"
 end
 
+rule "Primo VE Marc - Lsr26 surrogate"
+	when
+		MARC "598" has any "r" AND
+		MARC."598".ind"2"  equals "9"
+	then
+		set TEMP"1" to MARC "598" sub without sort "r"
+		create pnx."search"."lsr26" with TEMP"1"
+end
+
+rule "Primo VE Marc - Lsr26 component"
+	when
+		MARC "599" has any "8,r" AND
+		MARC."599".ind"2"  equals "9"
+	then
+		set TEMP"1" to MARC "599" sub without sort "8,r"
+		create pnx."search"."lsr26" with TEMP"1"
+end
 
 
