1eadc858cf51c97ce7071f259385a7bbe0e2937d
Corinna Baksik <cbaksik@gmail.com>
2025-09-22 11:14:49 -0400

    add subfield d for via recs

--- discovery-ve/lds44-associatedName.vb
+++ discovery-ve/lds44-associatedName.vb
@@ -2,7 +2,7 @@ rule "Primo VE - Lds44"
 	when
 		MARC is "720"."a"
 	then
-		create pnx."display"."lds44" with MARC "720" sub without sort "a,e"
+		create pnx."display"."lds44" with MARC "720" sub without sort "a,d,e"
 end
 
 rule "Primo VE - Lds06 - 880-720"
@@ -10,5 +10,5 @@ rule "Primo VE - Lds06 - 880-720"
         MARC is "880" AND
         MARC."880"."6" match "720-.*"
 	then
-        create pnx."display"."lds44" with MARC."880" sub without sort "a,e"
+        create pnx."display"."lds44" with MARC."880" sub without sort "a,d,e"
 end
