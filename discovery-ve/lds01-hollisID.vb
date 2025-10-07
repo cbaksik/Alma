ed6ed074ed7083fc6ae10c556450a31c26cd60e2
Corinna Baksik <cbaksik@gmail.com>
2025-10-07 13:32:27 -0400

    show via but not recap

--- discovery-ve/lds01-hollisID.vb
+++ discovery-ve/lds01-hollisID.vb
@@ -14,11 +14,12 @@ priority 90
 		create pnx."display"."lds01" with TEMP"1"
 end
 
-rule "Primo VE - Lds01 - VE external data"
+rule "Primo VE - Lds01 - VE external data -- show VIA but not recap"
 priority 80
 	when
 		MARC.control is "001" AND
-		MARC.control is "003"
+		MARC.control is "003" AND NOT
+		MARC is "852"
 	then
 		set TEMP"1" to MARC.control."001"
 		//set TEMP"2" to MARC.control."003"
@@ -27,6 +28,4 @@ priority 80
 		//concatenate with delimiter (TEMP"2",TEMP"1","")		
 		create pnx."display"."lds01" with TEMP"1"
 end
-
-
 
