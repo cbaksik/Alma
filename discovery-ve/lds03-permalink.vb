7ef753646b29424b770c4a14b1ec02bdb8f2a146
Corinna Baksik <cbaksik@gmail.com>
2025-10-07 13:32:18 -0400

    id and perma for via but not recap

--- discovery-ve/lds03-permalink.vb
+++ discovery-ve/lds03-permalink.vb
@@ -13,6 +13,20 @@ rule "Primo VE - Lds03"
 		create pnx."display"."lds03" with TEMP"1"
 end
 
+rule "Primo VE - Lds03 - VE external data -- show VIA but not recap"
+priority 80
+	when
+		MARC.control is "001" AND
+		MARC.control is "003" AND NOT
+		MARC is "852"
+	then
+		set TEMP"1" to MARC.control."001"
+		add prefix (TEMP"1","<a href=\"https://id.lib.harvard.edu/via/")
+		add suffix (TEMP"1","/catalog\">Permanent link to HOLLIS record</a>")
+		create pnx."display"."lds03" with TEMP"1"
+end
+
+
 
 // <a href="https://id.lib.harvard.edu/alma/990025484440203941/catalog">Permanent link to HOLLIS record</a>
 
