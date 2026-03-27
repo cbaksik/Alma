3ccfd0cdc44d22e1d486e9f8f3d8e51a869df852
Corinna Baksik <cbaksik@gmail.com>
2026-03-27 15:57:45 -0400

    remove f,l from uniform title hyperlinked searches

--- discovery-ve/uniformTitle.vb
+++ discovery-ve/uniformTitle.vb
@@ -3,7 +3,7 @@ rule "Primo VE - Uniform Title 130"
 		MARC is "130"
 	then
 		set TEMP"1" to MARC."130" excluding num subfields without sort
-		set TEMP"4" to MARC."130" sub without sort "a,d,g,k-t"
+		set TEMP"4" to MARC."130" sub without sort "a,d,g,h,k,m-t"
 		remove substring using regex (TEMP"4","(;|,|\\.)+$")
 		concatenate with delimiter (TEMP"1",TEMP"4","$$Q")
 		create pnx."display"."unititle" with TEMP"1"
@@ -13,15 +13,24 @@ rule "Primo VE - Uniform Title 240"
 	when
 		MARC is "240"
 	then
-		create pnx."display"."unititle" with MARC."240" excluding num subfields without sort
+		set TEMP"1" to MARC."240" excluding num subfields without sort
+		set TEMP"4" to MARC."240" sub without sort "a,d,g,h,k,m-t"
+		remove substring using regex (TEMP"4","(;|,|\\.)+$")
+		concatenate with delimiter (TEMP"1",TEMP"4","$$Q")
+		create pnx."display"."unititle" with TEMP"1"	
 end
 
 rule "Primo VE - Uniform Title 243"
 	when
 		MARC is "243"
 	then
-		create pnx."display"."unititle" with MARC."243" excluding num subfields without sort
+		set TEMP"1" to MARC."243" excluding num subfields without sort
+		set TEMP"4" to MARC."243" sub without sort "a,d,g,h,k,m-t"
+		remove substring using regex (TEMP"4","(;|,|\\.)+$")
+		concatenate with delimiter (TEMP"1",TEMP"4","$$Q")
+		create pnx."display"."unititle" with TEMP"1"	
 end
+
 rule "Primo VE - Alternate Uniform Title 880-130"
 	when
 		MARC is "880" AND
