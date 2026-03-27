b819c8300986b30846b48c0ab0e291d4d2300a1b
Corinna Baksik <cbaksik@gmail.com>
2026-03-27 15:57:01 -0400

    add 900 to lsr01 with and without leading zeros

--- discovery-ve/lds01-lsr01-hollisID.vb
+++ discovery-ve/lds01-lsr01-hollisID.vb
@@ -5,3 +5,26 @@ rule "Primo VE Marc - Lsr01"
 		set TEMP"1" to MARC.control."001"
 		create pnx."search"."lsr01" with TEMP"1"
 end
+
+rule "Primo VE Marc - 900"
+	when
+		MARC."900" has any "a"
+	then
+		set TEMP"1" to MARC."900" sub without sort "a"
+		set TEMP"2" to MARC."900" sub without sort "a"
+		remove substring using regex (TEMP"2","^0+")
+		concatenate with delimiter (TEMP"1",TEMP"2"," ")
+		create pnx."search"."lsr01" with TEMP"1"
+end
+
+rule "Primo VE Marc - 901"
+	when
+		MARC."901" has any "a"
+	then
+		set TEMP"1" to MARC."901" sub without sort "a"
+		create pnx."search"."lsr01" with TEMP"1"
+end
+
+
+
+
