35ea367e3b7d65bb893c153a41e84fba61c74d39
Corinna Baksik <cbaksik@gmail.com>
2026-04-09 10:28:49 -0400

    add searching for GIFT bookplates

--- discovery-ve/lds06-lsr06-bookplate.vb
+++ discovery-ve/lds06-lsr06-bookplate.vb
@@ -7,3 +7,13 @@ rule "Primo VE Marc - Lsr06"
 		replace string by string (TEMP"1","(.{3}).{8}(.{6}).*","$1_$2")
 		create pnx."search"."lsr06" with TEMP"1"
 end
+
+rule "Primo VE Marc - Lsr06 gift"
+	when
+		MARC is "911"."a" AND 
+		MARC."911"."a" match "GIFT.*"
+	then
+		set TEMP"1" to MARC."911"."a"
+		create pnx."search"."lsr06" with TEMP"1"
+end
+
