1afa9b0e25979bdefcdf707be09e34824a937869
Corinna Baksik <cbaksik@gmail.com>
2026-06-22 10:55:43 -0400

    Update lds35-microlanguage.vb

--- discovery-ve/lds35-microlanguage.vb
+++ discovery-ve/lds35-microlanguage.vb
@@ -1,37 +1,21 @@
 rule "Primo VE - Lds35"
 	when
-        MARC is "041"."a" AND
-	   MARC."041"."2" match "iso639-3"
+        MARC.control is "008"
 	then
-	    set TEMP"2" to MARC."041"."a"
-		remove substring using regex (TEMP"2","(ara|ben|ces|dan|deu|eng|fra|ita|jpn|kor|por|rus|spa|urd|zho)")
-        return list using regex (TEMP"1",TEMP"2","[a-z]{3}")
+		set TEMP"2" to MARC.control."008".Language
+		return list using regex (TEMP"1",TEMP"2","[a-z]{3}")
         add prefix to list (TEMP"1","lang.")
         create pnx."display"."lds35" with list TEMP"1"
 end
 
-rule "Primo VE - Lds35 sung"
+rule "Primo VE- Language 041 Lds35"
 	when
-        MARC is "041"."d" AND
-	   MARC."041"."2" match "iso639-3"
+		MARC."041" has any "a,d,e" 
 	then
-	    set TEMP"2" to MARC."041"."d"
+		set TEMP"2" to MARC."041" sub without sort "a,d,e"
+        lower case (TEMP"2")
         return list using regex (TEMP"1",TEMP"2","[a-z]{3}")
         add prefix to list (TEMP"1","lang.")
         create pnx."display"."lds35" with list TEMP"1"
 end
-
-rule "Primo VE - Lds35 libretto"
-	when
-        MARC is "041"."e" AND
-	   MARC."041"."2" match "iso639-3"
-	then
-	    set TEMP"2" to MARC."041"."e"
-        return list using regex (TEMP"1",TEMP"2","[a-z]{3}")
-        add prefix to list (TEMP"1","lang.")
-        create pnx."display"."lds35" with list TEMP"1"
-end
-
-
-
 
