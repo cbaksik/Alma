2999527d829e52b793ceb0eafb5d96624e222f89
Corinna Baksik <cbaksik@gmail.com>
2026-07-02 15:53:47 -0400

    Update lds35-lsr35-microlanguage.vb

--- discovery-ve/lds35-lsr35-microlanguage.vb
+++ discovery-ve/lds35-lsr35-microlanguage.vb
@@ -4,7 +4,7 @@ rule "Primo VE Marc - Lsr35"
 	   MARC."041"."2" match "iso639-3"
 	then
 	    set TEMP"2" to MARC."041"."a"
-		remove substring using regex (TEMP"2","(ara|ben|ces|dan|deu|eng|fra|ita|jpn|kor|por|rus|spa|urd|zho)")
+		remove substring using regex (TEMP"2","(ara|ben|ces|dan|deu|eng|fra|ita|jpn|kor|por|rus|spa|tur|urd|zho)")
         return list using regex (TEMP"1",TEMP"2","[a-z]{3}")
         add prefix to list (TEMP"1","lang.")
         create pnx."search"."lsr35" with list TEMP"1"
